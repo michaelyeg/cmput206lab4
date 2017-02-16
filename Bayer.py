6d38fd3f30bac95cb4a44443223dbb3624961703
bcvancouver <michael-xi@hotmail.com>
2017-02-16 02:30:36 -0700

    debugging part 1...

--- Bayer.py
+++ Bayer.py
@@ -8,7 +8,7 @@ from matplotlib import pyplot as plt
 
 img = cv2.imread('PeppersBayerGray.bmp', 0)
 
-h,w = img.shape
+h, w = img.shape
 
 # our final image will be a 3 dimentional image with 3 channels
 rgb = np.zeros((h,w,3),np.uint8);
@@ -21,13 +21,13 @@ IG = np.copy(img) # copy the image into each channel
 for row in range(0, h, 4): # loop step is 4 since our mask size is 4.
     for col in range(0, w, 4): # loop step is 4 since our mask size is 4.
         
-        IG[row, col+1] = (int(img[row,col])+int(img[row,col+2]))/2
+        IG[row, col+1] = (int(img[row, col])+int(img[row, col+2]))/2
         IG[row, col+3] = (int(img[row, col + 2]) + int(img[row + 1, col + 3])) / 2
 
         IG[row+1, col] = (int(img[row, col]) + int(img[row+2, col])) / 2
-        IG[row+1, col+2] = (int(img[row+1, col+1]) + int(img[row+1, col+3])+int(img[row, col+2])+int(img[row+2, col+2]))/ 2
+        IG[row+1, col+2] = (int(img[row+1, col+1]) + int(img[row+1, col+3])+int(img[row, col+2])+int(img[row+2, col+2]))/4
 
-        IG[row+2, col+1] = (int(img[row+2, col]) + int(img[row+2, col+2])+int(img[row+1, col+1])+int(img[row+3, col+1])) / 2
+        IG[row+2, col+1] = (int(img[row+2, col]) + int(img[row+2, col+2])+int(img[row+1, col+1])+int(img[row+3, col+1]))/4
         IG[row+2, col+3] = (int(img[row+1, col+3]) + int(img[row+3, col+3])) / 2
 
         IG[row+3, col] = (int(img[row+2, col]) + int(img[row+3, col+1])) / 2
@@ -76,10 +76,12 @@ for row in range(0, h, 4):
         IR[row + 3, col + 3] = int(img[row + 3, col + 2])
 
 # merge the channels
-rgb[:,:,0] = IR
-rgb[:,:,1] = IG
-rgb[:,:,2] = IB
-
+# Red Channel
+rgb[:, :, 0] = IR
+# Green Channel
+rgb[:, :, 1] = IG
+# Blue Channel
+rgb[:, :, 2] = IB
 
 
 cv2.imwrite('rgb.jpg', rgb)
