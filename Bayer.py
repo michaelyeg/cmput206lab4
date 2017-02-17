7a31f664b335ace27de3f7d2761800b45930172a
bcvancouver <michael-xi@hotmail.com>
2017-02-16 18:10:03 -0700

    debug completed

--- Bayer.py
+++ Bayer.py
@@ -11,7 +11,7 @@ img = cv2.imread('PeppersBayerGray.bmp', 0)
 h, w = img.shape
 
 # our final image will be a 3 dimensional image with 3 channels
-rgb = np.zeros((h,w,3),np.uint8);
+rgb = np.zeros((h,w,3),np.uint8)
 
 
 # reconstruction of the green channel IG
@@ -45,13 +45,13 @@ for row in range(0, h, 4):
         IR[row + 1, col + 3] = (int(img[row, col + 3]) + int(img[row + 2, col + 3])) / 2
         IR[row + 2, col + 2] = (int(img[row + 2, col+1])+int(img[row + 2, col + 3]))/2
 
-        IR[row, col] = int(img[row, col+1])
-        IR[row + 1, col] = int(img[row + 1, col + 1])
-        IR[row + 2, col] = int(img[row + 2, col + 1])
+        IR[row, col] = IR[row, col+1]
+        IR[row + 1, col] = IR[row + 1, col + 1]
+        IR[row + 2, col] = IR[row + 2, col + 1]
 
-        IR[row + 3, col + 1] = int(img[row + 2, col + 1])
-        IR[row + 3, col + 2] = int(img[row + 2, col + 2])
-        IR[row + 3, col + 3] = int(img[row + 2, col + 3])
+        IR[row + 3, col + 1] = IR[row + 2, col + 1]
+        IR[row + 3, col + 2] = IR[row + 2, col + 2]
+        IR[row + 3, col + 3] = IR[row + 2, col + 3]
         IR[row + 3, col] = IR[row + 3, col + 1]
 
 # reconstruction of the blue channel IB
@@ -60,20 +60,21 @@ IB = np.copy(img)
 
 for row in range(0, h, 4):
     for col in range(0, w, 4):
-        IR[row + 1, col + 1] = (int(img[row + 1, col])+int(img[row + 1, col + 2]))/2
-        IR[row + 2, col] = (int(img[row + 1, col])+int(img[row + 3, col]))/2
-        IR[row + 2, col + 2] = (int(img[row + 1, col])+int(img[row + 3, col]) + int(img[row + 1, col + 2]) + int(img[row + 3, col + 2]))/4
-        IR[row + 2, col + 2] = (int(img[row + 1, col + 2]) + int(img[row + 3, col + 2])) / 2
-        IR[row + 3, col + 3] = (int(img[row + 3, col])+int(img[row + 3, col + 2]))/2
+        IB[row + 1, col + 1] = (int(img[row + 1, col])+int(img[row + 1, col + 2]))/2
+        IB[row + 2, col] = (int(img[row + 1, col])+int(img[row + 3, col]))/2
+        IB[row + 2, col + 1] = (int(img[row + 1, col])+int(img[row + 3, col]) + int(img[row + 1, col + 2]) + int(img[row + 3, col + 2]))/4
+        IB[row + 2, col + 2] = (int(img[row + 1, col + 2]) + int(img[row + 3, col + 2])) / 2
+        IB[row + 3, col + 1] = (int(img[row + 3, col])+int(img[row + 3, col + 2]))/2
 
-        IR[row, col] = int(img[row + 1, col])
-        IR[row, col + 1] = int(img[row + 1, col + 1])
-        IR[row, col + 2] = int(img[row + 1, col + 2])
-        IR[row, col + 3] = IR[row, col + 2]
+        IB[row, col] = IB[row + 1, col]
+        IB[row, col + 1] = IB[row + 1, col + 1]
+        IB[row, col + 2] = IB[row + 1, col + 2]
+        IB[row, col + 3] = IB[row, col + 2]
 
-        IR[row + 1, col + 3] = int(img[row + 1, col + 2])
-        IR[row + 2, col + 3] = int(img[row + 2, col + 2])
-        IR[row + 3, col + 3] = int(img[row + 3, col + 2])
+        IB[row + 1, col + 3] = IB[row + 1, col + 2]
+        IB[row + 2, col + 3] = IB[row + 2, col + 2]
+        IB[row + 3, col + 3] = IB[row + 3, col + 2]
+
 
 # merge the channels
 # Red Channel
